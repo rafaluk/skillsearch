5941060eefcfb41e3bce935c999ef5ed8d53300d
rafalklat <rafal.klat@gmail.com>
2020-07-29 00:31:56 +0200

    Delete unused variables

--- utils/config.py
+++ utils/config.py
@@ -3,10 +3,10 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    pracuj_address = "https://www.pracuj.pl"
-    page_search_limit = 14
-    chrome_driver_path = "C:/Users/rafal.klat/chromedriver.exe"
+    page_search_limit: int = 6
+    chrome_driver_path: str = "C:/Users/rafal.klat/chromedriver.exe"
     # todo: fetch it from website automatically
-    pracuj_it_links = ["https://www.pracuj.pl/praca/it%20-%20rozw%C3%B3j%20oprogramowania;cc,5016"
-        # ,"https://www.pracuj.pl/praca/it%20-%20administracja;cc,5015"
-                       ]
+    pracuj_it_links: list = (
+        "https://www.pracuj.pl/praca/it%20-%20rozw%C3%B3j%20oprogramowania;cc,5016"
+        , "https://www.pracuj.pl/praca/it%20-%20administracja;cc,5015"
+    )
