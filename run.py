f8957cdb30eb72996474dffa4894d23304a19b0c
rafalklat <rafal.klat@gmail.com>
2020-07-28 23:58:57 +0200

    Extract phases to methods

--- run.py
+++ run.py
@@ -8,37 +8,53 @@ from utils.utils import calculate_time, read_skills, save_offer_output, save_ski
 
 @calculate_time
 def run():
-    """This will:
-        * prepare the driver for web scraping
-        * get links to all job offers from all pages in specified categories
-        * get content of every job offer
-        * search for specified technologies within all offers
-        """
-    # prepare chrome driver
+    """This will run all phases"""
+
+    # PHASE: DRIVER - prepare chrome driver
     driver = Driver().prepare(Config.chrome_driver_path)
 
-    # get job offers in Link data model
+    # PHASE: LINK EXTRACTION - get job offers in Link data model
+    job_links = run_link_extraction(driver)
+
+    # PHASE: OFFER EXTRACTION - get into an offer and get it's content in Offer data model
+    job_offers = run_offer_extraction(driver, job_links)
+
+    # PHASE: SKILL FINDING - get skills from offers in OfferWithSkills data model
+    job_offers_with_skills = run_skill_finding(job_offers)
+
+    # PHASE: SKILL AGGREGATION - aggregate skills into a dictionary of skills with quantities
+    run_skill_aggregation(job_offers_with_skills)
+
+
+@calculate_time
+def run_link_extraction(driver):
     link_extractor = LinkExtractor(driver=driver)
     job_links = link_extractor.get_all_links(Config.pracuj_it_links)
-
     print(f"Got {len(job_links)} job links.")
+    return job_links
 
-    # get into an offer and get it's content in Offer data model
+
+@calculate_time
+def run_offer_extraction(driver, job_links):
     offer_extractor = OfferExtractor(driver=driver)
     job_offers = offer_extractor.get_all_offers(job_links)
-
     print(f"Got {len(job_offers)} job offers.")
+    return job_offers
 
-    # get skills from offers in OfferWithSkills data model
-    skill_extractor = SkillFinder(skills=read_skills("skills.txt"))
-    job_offers_with_skills = skill_extractor.get_skills_for_all(job_offers)
 
+@calculate_time
+def run_skill_finding(job_offers):
+    skill_finder = SkillFinder(skills=read_skills("skills.txt"))
+    job_offers_with_skills = skill_finder.get_skills_for_all(job_offers)
     # save output to file
     save_offer_output(filename="outputs/offers_with_skills.csv", offers=job_offers_with_skills)
+    return job_offers_with_skills
 
+
+@calculate_time
+def run_skill_aggregation(job_offers_with_skills):
     skill_aggregator = SkillAggregator()
     skills = skill_aggregator.process_all_offers(job_offers_with_skills)
-
     # save output to file
     save_skills_output(filename="outputs/skills_with_quantities.csv", skills=skills)
 
